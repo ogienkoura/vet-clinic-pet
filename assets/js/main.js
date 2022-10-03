fdc3424fe4a9e3bc1c195156a8fd5d72b9502e06
Yura <pumpkteam3@gmail.com>
2022-10-03 23:49:19 +0300

    добавил слайдер spliderjs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,19 @@
-
-$(document).ready(function () {
-
-  $('.header-navbar__hamburger').on('click', function () {
-    $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
-  });
-
+var splide = new Splide('.splide', {
+  perPage: 3,
+  focus: 0,
+  omitEnd: true,
+  gap: 60,
+  arrowPath: 'M22.2564 4.57594L18.3777 0.666667L19.0392 0L24 5L19.0392 10L18.3777 9.33333L22.1624 5.51875L0 5.51875V4.57594L22.2564 4.57594Z'
 });
+
+splide.mount();
+
+// $(document).ready(function () {
+
+//   $('.header-navbar__hamburger').on('click', function () {
+//     $(this).toggleClass('active');
+//     $('.header-navbar__list').toggleClass('active');
+//   });
+
+// });
+
