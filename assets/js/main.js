d05db44f616e8f6d760ad357a864c888aedb2f0f
Yura <pumpkteam3@gmail.com>
2022-10-07 23:16:52 +0300

    сверстал секцию отзывы

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,23 @@ var splide = new Splide('.splide', {
   focus: 0,
   omitEnd: true,
   gap: 60,
+  pagination: false,
   arrowPath: 'M22.2564 4.57594L18.3777 0.666667L19.0392 0L24 5L19.0392 10L18.3777 9.33333L22.1624 5.51875L0 5.51875V4.57594L22.2564 4.57594Z'
 });
 
 splide.mount();
+
+var splide2 = new Splide('#slider2', {
+  perPage: 1,
+  pagination: false,
+  autoplay: true,
+  type: 'loop',
+  gap: 10,
+});
+
+splide2.mount();
+
+
 
 // $(document).ready(function () {
 
